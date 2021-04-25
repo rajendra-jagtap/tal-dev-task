75ec4802b6323f8a8e9b01df54f3d34cfc9f6611
raj8595 <root@ip-172-31-8-115.ec2.internal>
2021-04-25 05:57:27 +0000

    removed terraform installation setps in deploy script

--- task-3/deploy.sh
+++ task-3/deploy.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-# Install Terraform
-curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
-apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-apt-get update && sudo apt-get install terraform
-
 
 set -eou pipefail
 
