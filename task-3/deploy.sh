917513ca8d80faa241bb9873cfcf09ef0d0b3a32
raj8595 <root@ip-172-31-8-115.ec2.internal>
2021-04-25 05:37:21 +0000

    added terraform installation setps in deploy script

--- task-3/deploy.sh
+++ task-3/deploy.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+
+# Install Terraform
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install terraform
+
+
 set -eou pipefail
 
 /usr/local/bin/terraform init
